0ba0ae40a33423a32d6e504e40dd55ff59f78a9d
scc <songchengcheng@baijiahulian.com>
2019-12-04 17:07:19 +0800

    人员登记系统

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -26,6 +26,44 @@ export class FormComponent implements OnInit {
 
       mark:''
   }
+
+  public student:any = '我是一个学生';
+
+  public h = '<h4>这是一个 h2 用[innerHTML]来解析</h4>';
+
+  public id = '123';
+
+  public msg = '你好';
+
+  public arr = [
+    {name: '小1', age: 21},
+    {name: '小2', age: 22},
+    {name: '小3', age: 23},
+    {name: '小4', age: 23},
+  ];
+  public score = 2;
+
+  getData(){ /*自定义方法获取数据*/ //获取
+    alert(this.msg);
+  }
+  setData(){
+      //设置值
+      this.msg='这是设置的值';
+  }
+  keyUpFn(e){
+    console.log(e)
+  }
+  public flag=false;
+
+  public list = [1, 3, 4, 5, 6];
+
+  public attr='red';
+
+  public today=new Date();
+
+  public str = 'abcDEF';
+
+  public num = 123.456789;
 
   constructor() { }
 
